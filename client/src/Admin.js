f1a909e37e909463eaed7f5c121d87def0d5493f
Tim <timd1026@gmail.com>
2019-01-07 15:31:26 -0700

    Navbar Done

---
+++ client/src/Admin.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import './styles.css'
+
+class Admin extends Component {
+    constructor(){
+        super()
+        this.state = {
+            
+        }
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Admin
