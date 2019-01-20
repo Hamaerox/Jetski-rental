e30a99bb0438debd2ee92f6768b863b692254b72
Hamaerox <timd1026@gmail.com>
2019-01-20 18:16:10 -0500

    heroku deployment

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressJwt = require("express-jwt");
 const PORT = process.env.PORT || 8000
+const path = require("path")
 
 app.use(express.json()) 
 app.use(morgan('dev'))  
 app.use("/api", expressJwt({secret: process.env.SECRET})) //req.user === {username, password, _id}
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 
 //Routes
@@ -31,7 +33,9 @@ app.use((err, req, res, next) => {
     return res.send({errMsg: err.message})
 })
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT} sir!`)
